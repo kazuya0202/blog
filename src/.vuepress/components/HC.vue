c5895cbe58da049873171d3f3980d01b617a3614
ichiya <kazuya7187@gmail.com>
2019-12-22 17:29:18 +0900

    update

---
+++ src/.vuepress/components/HC.vue
@@ -0,0 +1,19 @@
+<!-- HC: head content -->
+
+<script>
+export default {
+    functional: true,
+    props: {
+        text: String,
+    },
+    render (h, { props, slots }) {
+        if (props.text === undefined || props.text === '') {
+            props.text = '◆'
+        }
+
+        return h('span', {
+            class: ['head-content'],
+        }, props.text || slots().default)
+    }
+}
+</script>
